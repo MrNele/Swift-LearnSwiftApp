986d82d91347ef80e59df04d6b0f1d0e04c77550
Nemanja Velimirovic <velimirovicnemanja@yahoo.com>
2021-10-20 14:30:29 +0200

    Initial Commit

---
+++ LearnSwiftApp/LearnSwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftAppApp.swift
+//  LearnSwiftApp
+//
+//  Created by iMac on 20.10.21..
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
