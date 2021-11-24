4fb4f9ef98ea7a6b91d043afdf3b778ca6018836
Nemanja Velimirovic <velimirovicnemanja@yahoo.com>
2021-11-24 17:54:20 +0100

    - added TestResultView - show TestResultView (result of test) in the TestView after the last question

---
+++ LearnSwiftApp/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  LearnSwiftApp
+//
+//  Created by iMac on 23.11.21..
+//
+
+import UIKit
+import Firebase
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions:
+                   [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
--- LearnSwiftApp/LearnSwiftApp.swift
+++ LearnSwiftApp/LearnSwiftApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct LearningApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             HomeView()
